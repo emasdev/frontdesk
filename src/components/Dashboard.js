ab2841b6498faaf7546371d793c69cf5abd8d655
Emas <emas.dev@gmail.com>
2021-11-16 10:57:14 -0600

    add eventos in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect, createContext } from 'react';
 import Lista from './Lista';
 import { Box } from '@chakra-ui/react';
 import NuevoPacienteBtn from './NuevoPacienteBtn';
+import Eventos from './Eventos';
 
 export default function Dashboard() {
   return (
     <Box>
       <NuevoPacienteBtn />
-      <Lista />
+      <Eventos />
     </Box>
   );
 }
---
+++ src/components/Eventos.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useContext } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import AppContext from '../context/AppContext';
+import moment from 'moment';
+
+export default function Eventos() {
+  const { eventos } = useContext(AppContext);
+  return (
+    <>
+      <Table size="sm">
+        <Thead>
+          <Tr>
+            <Th>Hora</Th>
+            <Th>Paciente</Th>
+            <Th>Doctor</Th>
+            <Th>Total</Th>
+            <Th>Pago</Th>
+            <Th>Estado</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {eventos &&
+            eventos.map(evento => {
+              console.log(evento);
+              return (
+                <Tr key={evento.id}>
+                  <Td>{moment(evento.start).format('hh:mm')}</Td>
+                  <Td>
+                    {evento.extendedProps.nombre}{' '}
+                    {evento.extendedProps.apellidos}
+                  </Td>
+                  <Td>
+                    {evento.extendedProps.doctor.nombre}{' '}
+                    {evento.extendedProps.doctor.apellido_paterno}{' '}
+                    {evento.extendedProps.doctor.apellido_materno}
+                  </Td>
+                  <Td>Total</Td>
+                  <Td>Pago</Td>
+                  <Td>Estado</Td>
+                </Tr>
+              );
+            })}
+        </Tbody>
+      </Table>
+    </>
+  );
+}
